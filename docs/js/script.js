6cac9149e1f53f5f38baeffaee96937b1c1ba31d
Dr Flizzle <145814683+WalnutWalrus@users.noreply.github.com>
2023-10-07 21:27:39 +1300

    Rough outline for site

--- docs/js/script.js
+++ docs/js/script.js
@@ -10,4 +10,20 @@ $(document).ready(function() {
 
     // Load welcome tab by default
     loadTabContent('welcome');
+
+    $('#yourTableID').DataTable({
+        responsive: true,
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'excel', 'pdf'
+        ],
+        // Add this to use Bootstrap 5 styles
+        "pagingType": "full_numbers",
+        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+        responsive: true,
+        language: {
+            search: "_INPUT_",
+            searchPlaceholder: "Search records",
+        }
+    });
 });
